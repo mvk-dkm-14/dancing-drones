1cdce97dddaba7cdd759f03c082a7653b13ff3c2
Christian Avemark <chav02@kth.se>
2014-04-29 15:09:49 +0200

    Add droneshow class.

---
+++ DancingDronesJava/src/DroneShow.java
@@ -0,0 +1,46 @@
+import java.sql.Time;
+
+import dancingdrones.common.Protocol;
+
+
+public class DroneShow {
+	private DroneCommand top;
+	private DroneCommand root;
+	private DroneCommand current;
+	private int size;
+	
+	public DroneShow() {
+		super();
+		size = 0;
+	}
+	
+	public void add(Time time, Protocol.Value command){
+		root = new DroneCommand(time, command, top);
+		if( size == 0){
+			current = top;
+			root = top;
+		}
+		size++;
+	}
+
+	public void reset(){
+		current = root;
+	}
+	
+	public DroneCommand next(){
+		DroneCommand tmp = current;
+		current = current.next;
+		return tmp;
+	}
+	public class DroneCommand {
+		private Time timing;
+		private Protocol.Value command;
+		private DroneCommand next;
+		
+		public DroneCommand(Time timing, Protocol.Value command, DroneCommand next) {
+			this.command = command;
+			this.timing = timing;
+			this.next = next;
+		}
+	}
+}
